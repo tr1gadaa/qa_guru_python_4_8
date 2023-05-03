92dcf5944856269495ea67de8d687ca659325ab5
tr1gadaa <118889981+tr1gadaa@users.noreply.github.com>
2023-05-03 22:29:50 +0300

    add tests cart

--- homework/test_shop.py
+++ homework/test_shop.py
@@ -10,6 +10,9 @@ from homework.models import Product, Cart
 def product():
     return Product("book", 100, "This is a book", 1000)
 
+@pytest.fixture
+def cart(product):
+    return Cart()
 
 class TestProducts:
 
@@ -35,3 +38,22 @@ class TestCart:
         На некоторые методы у вас может быть несколько тестов.
         Например, негативные тесты, ожидающие ошибку (используйте pytest.raises, чтобы проверить это)
     """
+
+    def test_add_product(self, cart, product):
+        cart.add_product(product, 999)
+        assert cart.products[product] == 999
+
+    def test_add_product_twice(self, cart, product):
+        cart.add_product(product, 1)
+        cart.add_product(product, 100)
+        assert cart.products[product] == 101
+
+    def test_remove_product_from_cart(self, cart, product):
+        cart.add_product(product, 101)
+        cart.remove_product(product, 1)
+        assert cart.products[product] == 100
+
+    def test_remove_all_product_from_cart(self, cart, product):
+        cart.add_product(product, 101)
+        cart.remove_product(product, 102)
+        assert cart.products[product] == 0
